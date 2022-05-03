36eef97d3ad5645267ecbc87807947b0df3349b8
exxidae7 <cuongxuando209@gmail.com>
2022-05-04 05:09:37 +0700

    jwt token and router

--- app/main.py
+++ app/main.py
@@ -2,15 +2,17 @@
 
 from fastapi import FastAPI ,Response , status, HTTPException , Depends
 from fastapi.params import Body
-from typing import Optional
+from typing import Optional , List
 from pydantic import BaseModel 
 from random import randrange
+
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-from . import models , schemas
+from . import models , schemas , utils
 from .database import engine ,SessionLocal , get_db
 from sqlalchemy.orm import Session 
+from .routes import post, user , auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -43,88 +45,12 @@ def find_index_post(id):
         if p['id'] == id:
             return i
         
-@app.get("/") 
-def read_root():
-    return {"hello" : "world"}
 
+app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(auth.router)
 
-@app.get("/posts")
-def get_post(db : Session = Depends(get_db)):
-    # cursor.execute("""
-    #                SELECT  * FROM posts
-    #                """) 
-    # posts = cursor.fetchall()
-    # print(posts)
-    posts = db.query(models.Post).all()
-    return  posts
+@app.get('/')
+def index():
+    return {"hello world"}
 
-
-@app.post("/createposts", status_code = status.HTTP_201_CREATED)
-def create_post(post: schemas.PostCreate , db : Session = Depends(get_db)):
-    # cursor.execute(""" INSERT INTO posts (title , content ,published) VALUES (%s,%s ,%s) RETURNING * """,
-    #                (post.title , post.content , post.published))
-    # new_post= cursor.fetchone()
-    # conn.commit()
-    new_post = models.Post(**post.dict())
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    
-    return new_post 
-
-@app.get("/post/latest")
-def get_latest_post():
-    post= my_post[len(my_post)  -1]
-    return  post
-
-@app.get("/post/{id}")
-def get_post(id : int , db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts WHERE id = %s """ , (str(id),))
-    # post = cursor.fetchone()
-    
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail = f"post with id : {id} was not found ")
-  
-    return  post
-
-
-@app.delete("/post/{id}" , status_code = status.HTTP_204_NO_CONTENT)
-def delete_post(id : int , db : Session = Depends(get_db)):
-    # cursor.execute("""DELETE FROM posts WHERE id = %s returning *""" , (str(id),))
-    # delete_post = cursor.fetchone()
-    # conn.commit()
-    post = db.query(models.Post).filter(models.Post.id == id)
-    if post.first() ==None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail = f"post with  id : {id} does not exist")
-    post.delete(synchronize_session=False)
-    db.commit()
-    
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-@app.put("/post/{id}")
-def update_post(id : int ,  updated_post  : schemas.PostCreate , db : Session = Depends(get_db)) :
-    # cursor.execute("""UPDATE 
-    #                posts 
-    #                SET 
-    #                title = %s, content = %s , published = %s 
-    #                WHERE id = %s
-    #                RETURNING *""", 
-    #                (post.title , post.content, post.published ,(str(id) , )))
-    # updated_post = cursor.fetchone()
-    # conn.commit()
-    post_query = db.query(models.PostCreate).filter(models.Post.id == id)  
-    post = post_query.first()
-    
-    if post ==None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail = f"post with  id : {id} does not exist")
-        
-    post_query.update(updated_post.dict() , synchronize_session = False)
-    
-    db.commit()
-
-    return  post_query.first()
--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,7 @@
 
-from pydantic import BaseModel
-
+from pydantic import BaseModel , EmailStr
+from datetime import datetime
+from typing import Optional
 class PostBase(BaseModel):
     
     title : str
@@ -12,8 +13,37 @@ class PostCreate(PostBase):
     pass
 
 
-class Post(BaseModel):
+class Post(PostBase):
     
-    title : str
-    content : str
-    published : bool = True
+    id : int
+    created_at : datetime
+    
+    class Config:
+        orm_mode = True
+ 
+        
+        
+class UserCreate(BaseModel):
+    email: EmailStr
+    password : str
+
+class Useroutput(BaseModel):
+    id : int
+    email: EmailStr
+    created_at : datetime
+    
+    class Config:
+        orm_mode = True
+        
+        
+class UserLogin(BaseModel):
+    email : EmailStr
+    password : str
+    
+    
+class Token(BaseModel):
+    access_token : str
+    token_type : str
+    
+class TokenData(BaseModel):
+    id : Optional[str] = None
